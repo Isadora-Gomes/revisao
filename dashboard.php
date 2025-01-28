b187826b4e80d185f261758943a4836cff6f06ad
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2025-01-28 11:56:40 -0300

    2

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,25 @@
     <title>Document</title>
 </head>
 <body>
-    
+<form action="processar.php" method="POST">
+        <label for="categoria">a</label>
+        <select name="categoria" id="categoria">
+            <?php
+            include 'conexao.php';
+            $sql = "SELECT id, nome FROM categorias";
+            $result = $conexao->query($sql);
+            echo "<script> console.log('c')</script>";
+            if ($result && $result->num_rows > 0) {
+                echo "<script> console.log('a')</script>";
+                while ($row = $result->fetch_assoc()) {
+                    echo "<script> console.log('b')</script>";
+                    echo '<option value="' . $row['nome_usuario']. '</option>';
+                }
+            } else {
+                echo '<option value="">Nenhuma categoria encontrada</option>';
+            }
+            ?>
+        </select>
+    </form>
 </body>
 </html>
