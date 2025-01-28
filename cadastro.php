2ad1acfbd9626bb3e97881c50caa7e5514caa580
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2025-01-28 11:09:36 -0300

    1

---
+++ cadastro.php
@@ -0,0 +1,96 @@
+<?php
+include 'conexao.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $var_nome = $_POST["nome"];
+    $var_email = $_POST["email"];
+    $var_senha = $_POST["senha"];
+
+    $senha_hashed = password_hash($var_senha, PASSWORD_DEFAULT);
+
+    $sql_insercao = "INSERT INTO usuarios (nome_usuario, email_usuario, senha_usuario) VALUES (?, ?, ?)";
+    
+    $stmt = $conexao->prepare($sql_insercao);
+    
+    $stmt->bind_param("sss", $var_nome, $var_email, $senha_hashed);
+    
+    if ($stmt->execute()) {
+        echo '<script>
+                document.addEventListener("DOMContentLoaded", function() {
+                    Swal.fire({
+                        text: "Cadastro efetuado com sucesso!",
+                        icon: "success"
+                    }).then(() => {
+                        window.location = "index.php";
+                    });
+                });
+              </script>';
+    } else {
+        echo '<script>
+                document.addEventListener("DOMContentLoaded", function() {
+                    Swal.fire({
+                        text: "Erro ao cadastrar usuário: ' . $conexao->error . '",
+                        icon: "error"
+                    });
+                });
+              </script>';
+    }
+
+    $stmt->close();
+    $conexao->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./img/HW-icon.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&family=Plus+Jakarta+Sans:wght@200..800&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/login.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <title>Cadastrar - História na Web</title>
+</head>
+<body>
+<section class="secao-cadastro">
+    <div class="box-cadastro">
+        <form action="" method="POST">
+            <h1>Cadastro</h1>
+            <label for="nome">Nome</label>
+            <input class="campo-inserir" type="text" name="nome" required>
+            
+            <label for="email">Email</label>
+            <input class="campo-inserir" type="email" name="email" required>
+            
+            <label for="senha">Senha</label>
+            <input class="campo-inserir" id="senha-campo" type="password" name="senha" required>
+            <div id='mostrar'>
+                <input type='checkbox' onclick='mostrarSenha()'> Mostrar senha
+            </div>
+            
+            <button id="botao-cadastrar" type="submit">Cadastrar</button>
+            <p class="celular">Já tem uma conta? <a href="./login.php" id="entre">Entre!</a></p>
+        </form>
+    </div>
+    <div class="box-bemvindo">
+        <h2>Já tem uma conta?</h2>
+        <p>Para continuar sua jornada conosco, entre com suas credenciais.</p>
+        <button id="botao-entrar" onclick="window.location='login.php'"><a href="#">Entrar</a></button>
+    </div>
+</section>
+<script>
+    function mostrarSenha() {
+        var x = document.getElementById("senha-campo");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+</script>
+</body>
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./img/HW-icon.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&family=Plus+Jakarta+Sans:wght@200..800&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/login.css">
+    <title>Login</title>
+</head>
+<body>
+    <section id="secao-login">
+        <div id="box-login">
+                <h1>Entre na sua conta</h1>
+                <form action="" method="POST">
+                    <label for="email">Email</label>
+                    <input class="inserir" type="email" name="email" required>
+
+                    <label for="senha">Senha</label>
+                    <input id="senha-campo" class="inserir" type="password" name="senha" required>
+                    <div id='mostrar'>
+                        <input type='checkbox' onclick='mostrarSenha()'> Mostrar senha
+                    </div>
+
+                     <?php
+
+                        session_start();
+                        include 'conexao.php';
+
+                        if($_SERVER["REQUEST_METHOD"] == "POST") {
+                            $var_email = $_POST["email"];
+                            $var_senha = $_POST["senha"];
+
+                            $query = "SELECT * FROM usuarios WHERE email_usuario = '$var_email'";
+
+                            $result = mysqli_query($conexao, $query);
+
+                            if($result->num_rows > 0) {
+                                $usuario_logado = $result->fetch_assoc();
+
+                                if (password_verify($var_senha, $usuario_logado['senha_usuario'])){
+                                    $_SESSION['id'] = $usuario_logado['id_usuario'];
+                                    $_SESSION['nome'] = $usuario_logado['nome_usuario'];
+                                    $_SESSION['email'] = $usuario_logado['email_usuario'];
+                                    echo "<script> console.log('a')</script>";
+                                    header('Location: dashboard.php?');
+                                } else {
+                                    echo "<p style='color:red;'>Senha incorreta</p>";
+                                }
+                            } else {
+                                echo "<p style='color:red;'>Email incorreto</p>";
+                            }
+                        }
+                        $conexao->close();
+                    ?> 
+
+                    <button id="entrar" type="submit">Entrar</button>
+                    <p class="celular">Não tem uma conta? <a href="./cadastro.php">Cadastre-se!</a></p>
+                </form>
+        </div>
+        <div id="box-welcome">
+            <h2>Não tem uma conta?</h2>
+            <p>Entre com seus dados pessoais e comece sua jornada conosco</p>
+            <button id="cadastrar-button" onclick="window.location='./cadastro.php'"><a href="./cadastro.php">Cadastre-se</a></button>
+        </div>
+    </div>
+</section>
+<script>
+    function mostrarSenha() {
+        var x = document.getElementById("senha-campo");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+</script>
+</body>
+</html>
